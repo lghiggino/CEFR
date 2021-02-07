c64b4902885423439ab972850168f3044181f327
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-07 11:33:19 -0300

    index images and remoteConfig details

--- public/js/remoteConfig.js
+++ public/js/remoteConfig.js
@@ -5,9 +5,10 @@ remoteConfig.settings = {
 remoteConfig.defaultConfig = ({
     'welcome_message': 'Remote Config Default Configuration is on',
     // background images
-    "mainSliderBackgroundImage": `url("images/bg/IMG_0091flip.jpg")`,
+    "mainSliderBackgroundImage": `url("images/bg/IMG_0046_resized-.jpg")`,
     "secondSliderBackgroundImage": `url("images/bg/pexels-cottonbro-4098224.jpg")`,
-    "especialidadesBackgroundImage" : `url("images/bg/pexels-cottonbro-4100663.jpg")`,
+    "especialidadesBackgroundImage" : `url("images/bg/IMG_0074_resized-.jpg")`,
+    "lacanBackgroundImage" : `url("images/bg/lacan2-1024x702.jpg")`,
     "textosBackgroundImage": `url("images/bg/pexels-prateek-katyal-2740956.jpg")`,
     "foreignBackgroundImage": `url("images/bg/woman-holding-hands-together-talking-with-counselor.jpg")`,
   });
@@ -23,6 +24,8 @@ const secondSliderBackgroundImage = document.querySelector("#secondSliderBackgro
 secondSliderBackgroundImage.style.backgroundImage = remoteConfig.defaultConfig.secondSliderBackgroundImage;
 const especialidadesBackgroundImage = document.querySelector("#especialidades");
 especialidadesBackgroundImage.style.backgroundImage = remoteConfig.defaultConfig.especialidadesBackgroundImage;
+const lacanBackgroundImage = document.querySelector("#lacan");
+lacanBackgroundImage.style.backgroundImage = remoteConfig.defaultConfig.lacanBackgroundImage;
 const textosBackgroundImage = document.querySelector("#textos");
 textosBackgroundImage.style.backgroundImage = remoteConfig.defaultConfig.textosBackgroundImage;
 const englishForeignBackgroundImage = document.querySelector("#english");
